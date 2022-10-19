a93d311452f19daef9bd291a0ad02e8ac20a827d
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-10-19 17:56:20 +0530

    conditional sta..

---
+++ 3_Operators/src/loveDSA/$5AssignmentOper.java
@@ -0,0 +1,21 @@
+// A=B , = is assignment oper
+// B = B - 5  =>  B -= 5
+// b = b + 12 ->  b+=12;
+package loveDSA;
+
+public class $5AssignmentOper {
+	public static void main(String[] args) {
+		int i =20;
+		i+= 40;
+		
+		int b = 20;
+		b -= 10;
+		
+		int c = 20;
+		c  /= 5;
+		
+		System.out.println(i);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
