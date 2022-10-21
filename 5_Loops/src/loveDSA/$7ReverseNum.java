4dc0a1defb1bdd1bdfdbef0c8207734cddff82db
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-10-21 12:09:07 +0530

    rev end

--- 5_Loops/src/loveDSA/$7ReverseNum.java
+++ 5_Loops/src/loveDSA/$7ReverseNum.java
@@ -1,16 +1,17 @@
 
 // 12345
 //54321
-//last digit concept : if we devide any num by 10 then we will get rem that is last digit
+//last digit concept : if we devide any num by 10 then we will get rem that is last digit 1234%10 -> 4
+//remoive last digit  num/10( 1234/10 -> 123)
 package loveDSA;
 
 public class $7ReverseNum {
 		public static void main(String[] args) {
 			int num = 12345;
 			while (num>0) {
-				int ld = num%10;
-				System.out.print(ld);
-				num = num/10;
+				int ld = num%10;  //getting last digit
+				System.out.print(ld); // printing last digit
+				num = num/10; // removing last digit every time
 			}
 //			System.out.println();
 		}
--- 5_Loops/src/loveDSA/$8ReverseNum.java
+++ 5_Loops/src/loveDSA/$8ReverseNum.java
@@ -2,13 +2,13 @@ package loveDSA;
 
 public class $8ReverseNum {
 public static void main(String[] args) {
-	int num = 12345;
-	int i = 10;
-	while( i>0) {
+	int num = 123456789;
+	int reverse = 0;
+	while( num>0) {
 		int ld = num%10;
-		i = (i* 10) + ld;
+		reverse= (reverse* 10) + ld;  // rev = (rev *10) + last digit
 		num = num/10;
 	}
-	System.out.println(num);
+	System.out.print("  "+reverse);
 }
 }
