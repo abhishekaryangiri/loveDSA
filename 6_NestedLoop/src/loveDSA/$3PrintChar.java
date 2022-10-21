3d139ab2f1d5c5b0366fd1265c63cfc33ed7fb05
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-10-21 14:50:57 +0530

    do more prob..

--- 6_NestedLoop/src/loveDSA/$3PrintChar.java
+++ 6_NestedLoop/src/loveDSA/$3PrintChar.java
@@ -7,10 +7,9 @@ public class $3PrintChar {
 		for( int i=1; i<=chakra; i++) {
 			for( int j=1; j<=i; j++) {
 				System.out.print(alpha );
-//				alpha++;
+				alpha++;
 			}
 			System.out.println();
 		}
 	}
-
 }
