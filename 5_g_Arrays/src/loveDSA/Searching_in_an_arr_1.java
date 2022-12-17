6f106e75208e22013bc339f1bc0cba90a4deea07
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-12-17 23:51:02 +0530

    2dArrayBasic

---
+++ 5_g_Arrays/src/loveDSA/Searching_in_an_arr_1.java
@@ -0,0 +1,15 @@
+package loveDSA;
+
+public class Searching_in_an_arr_1 {
+public static void main(String[] args) {
+	int arr[]= {1, 2 ,3, 4, 5, 6, 7, 8};
+	//int arr[];
+	for(int i=0; i<arr; i++) {
+		if(arr[i]==4) {
+			System.out.println("Element Found");
+		}else {
+			System.out.println("Element not Found");
+		}
+	}
+}
+}
--- 8_Pattern/src/loveDSA/$1RecShape.java
+++ 8_Pattern/src/loveDSA/$1RecShape.java
@@ -6,10 +6,10 @@ public static void printRec(int row, int col) {
 	for ( int i=1; i<=row; i++) {
 		//inner col
 		for( int j=1; j<=col; j++) {
-			if(i==1 || i== row || j==1 || j== col) {
+			if(i ==1 || i== row || j==1 || j == col) {
 				System.out.print("*");
 			}else {
-				System.out.print(" ");
+				System.out.print("");
 			}
 		}
 		System.out.println();
