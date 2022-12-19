eecb4cac516a4064d799c1b45092c9dc115d7a4a
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-12-19 23:56:05 +0530

    fix error

--- 5_g_Arrays/src/loveDSA/RearrangeArrApr1.java
+++ 5_g_Arrays/src/loveDSA/RearrangeArrApr1.java
@@ -1,10 +1,10 @@
 package loveDSA;
 
-public class Searching_in_an_arr_1 {
+public class RearrangeArrApr1 {
 public static void main(String[] args) {
 	int arr[]= {1, 2 ,3, 4, 5, 6, 7, 8};
 	//int arr[];
-	for(int i=0; i<arr; i++) {
+	for(int i=0; i<arr.length; i++) {
 		if(arr[i]==4) {
 			System.out.println("Element Found");
 		}else {
