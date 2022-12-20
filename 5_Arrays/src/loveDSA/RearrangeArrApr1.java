e87719a91d8d6e7964fd258b40e5655cea270497
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-12-20 23:06:27 +0530

    print pos int

---
+++ 5_Arrays/src/loveDSA/RearrangeArrApr1.java
@@ -0,0 +1,15 @@
+package loveDSA;
+
+public class RearrangeArrApr1 {
+public static void main(String[] args) {
+	int arr[]= {1, 2 ,3, 4, 5, 6, 7, 8};
+	//int arr[];
+	for(int i=0; i<arr.length; i++) {
+		if(arr[i]==4) {
+			System.out.println("Element Found");
+		}else {
+			System.out.println("Element not Found");
+		}
+	}
+}
+}
--- BitManipulation/src/loveDSA/BitwiseOperators2.java
+++ BitManipulation/src/loveDSA/BitwiseOperators2.java
@@ -1,5 +1,8 @@
 package loveDSA;
 
 public class BitwiseOperators2 {
-
+	 public static void main(String args[]) {
+	        int a = 0;
+	        System.out.println(~a);
+	    }
 }
