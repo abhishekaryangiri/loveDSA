b8610ca470fbf2355fd091c79d6bde1bd4e19e36
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-12-22 00:00:36 +0530

    last index

--- 9_String/src/loveDSA/LastIndex5.java
+++ 9_String/src/loveDSA/LastIndex5.java
@@ -1,5 +1,18 @@
+/*
+ Int lastIndexOf( String s): Returns the index within the string of the last occurrence of the specified string.
+ */
 package loveDSA;
 
 public class LastIndex5 {
+	 // Initialising String
+	public static void main(String[] args) {
+	
+    String Str = new String("Welcome to geeksforgeeks");
 
+    System.out.print("Found Last Index of g at : ");
+
+    // Last index of 'g' will print
+    // prints 19
+    System.out.println(Str.lastIndexOf('g'));
+}
 }
