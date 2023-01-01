7eab135287d32132f7162ce22fb1855c64412f3e
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-01-01 23:18:19 +0530

    star pattern

--- 6_NestedLoop/src/loveDSA/$3PrintChar.java
+++ 6_NestedLoop/src/loveDSA/$3PrintChar.java
@@ -2,12 +2,12 @@ package loveDSA;
 
 public class $3PrintChar {
 	public static void main(String[] args) {
-		char alpha = 'A';
+		char abhi = 'A';
 		int chakra = 6;
 		for( int i=1; i<=chakra; i++) {
 			for( int j=1; j<=i; j++) {
-				System.out.print(alpha );
-				alpha++;
+				System.out.print(abhi );
+				abhi++;
 			}
 			System.out.println();
 		}
