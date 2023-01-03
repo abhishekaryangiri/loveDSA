e1ec15a19d06b27e1435cd5961040ee5ad0651ea
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-01-03 23:21:41 +0530

    rev arr

---
+++ InterviewDSA_Array/src/javaYogi/ReverseTheArray2.java
@@ -0,0 +1,44 @@
+package javaYogi;
+
+public class ReverseTheArray2 {
+//	Write a program to reverse an array or string
+	/*
+Input :  	arr[] = {4, 5, 1, 2}
+Output : arr[] = {2, 1, 5, 4}
+	 */
+	
+	 /* Function to reverse arr[] from
+    start to end*/
+    static void rvereseArray(int arr[],
+                    int start, int end)
+    {
+        int temp;
+          
+        while (start < end)
+        {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }    
+      
+    /* Utility that prints out an
+    array on a line */
+    static void printArray(int arr[],
+                            int size)
+    {
+        for (int i = 0; i < size; i++)
+             System.out.print(arr[i] + " ");
+          
+         System.out.println();
+    }
+	public static void main(String[] args) {
+		 int arr[] = {1, 2, 3, 4, 5, 6};
+	        printArray(arr, 6);
+	        rvereseArray(arr, 0, 5);
+	        System.out.print("Reversed array is \n");
+	        printArray(arr, 6);
+	}
+}
