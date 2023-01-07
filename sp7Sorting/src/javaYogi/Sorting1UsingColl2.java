ebd9c15d4bd8a8b1000d62d052996fa3853117e4
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-01-07 23:31:38 +0530

    so..

--- sp7Sorting/src/javaYogi/Sorting1UsingColl2.java
+++ sp7Sorting/src/javaYogi/Sorting1UsingColl2.java
@@ -1,14 +1,9 @@
-/*
- Way 3: Using sort() method of Collections class 
-
-Collections.sort() works for objects Collections like ArrayList and LinkedList.
- */
 package javaYogi;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Sorting1UsingColl {
+public class Sorting1UsingColl2 {
 public static void main(String[] args) {
 	 // Create a list of strings
     ArrayList<String> al = new ArrayList<String>();
@@ -20,9 +15,9 @@ public static void main(String[] args) {
 
     /* Collections.sort method is sorting the
     elements of ArrayList in ascending order. */
-    Collections.sort(al);
+    Collections.sort(al, Collections.reverseOrder());
 
     // Let us print the sorted list
-    System.out.println("List after the use of"  + " Collection.sort() :\n" + al);
+    System.out.println("List after the use of" + " Collection.sort() :\n" + al);
 }
 }
