b114c742be36caec106e3a77bc09c648f33b844a
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-01-08 23:57:40 +0530

    org

--- sp7Sorting/src/javaYogi/Sorting2ReverseOrd.java
+++ sp7Sorting/src/javaYogi/Sorting2ReverseOrd.java
@@ -1,5 +1,24 @@
 package javaYogi;
 
-public class Sorting2ReverseOrd {
+import java.util.ArrayList;
+import java.util.Comparator;
 
+public class Sorting2ReverseOrd {
+	 public static void main(String[] args) {
+
+		    // create an ArrayList
+		    ArrayList<String> languages = new ArrayList<>();
+
+		    // add elements to ArrayList
+		    languages.add("Python");
+		    languages.add("Swift");
+		    languages.add("C");
+		    languages.add("JavaScript");
+		    System.out.println("Unsorted ArrayList: " + languages);
+
+		    // sort the ArrayList in ascending order
+		    languages.sort(Comparator.reverseOrder());
+
+		    System.out.println("Sorted ArrayList: " + languages);
+		  }
 }
