7c700fb98fd3faab10453f85fc4d89c957a1ad7f
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-01-09 23:15:15 +0530

    quick sort

--- sp7Sorting/src/javaYogi/Sorting2a.java
+++ sp7Sorting/src/javaYogi/Sorting2a.java
@@ -1,5 +1,16 @@
 package javaYogi;
 
-public class Sorting2a {
+import java.util.Arrays;
 
+public class Sorting2a {
+public static void main(String[] args) {
+	 //define an array
+    int[] intArray = {52, 45, 32, 64, 12, 87, 78, 98, 23, 7}; 
+
+    System.out.printf("Original Array : %s", Arrays.toString(intArray)); 
+
+    Arrays.sort(intArray); 
+
+    System.out.printf("\n\nSorted Array : %s", Arrays.toString(intArray));
+}
 }
