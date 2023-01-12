d2f8b004c12efc141e08596306f1bc6fb6cff242
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-01-12 23:53:57 +0530

    shell

---
+++ sp7Sorting/src/javaYogi/ShellSort17.java
@@ -0,0 +1,37 @@
+package javaYogi;
+
+import java.util.Arrays;
+
+public class ShellSort17 {
+	  public static void shell(int[] a) {
+			int increment = a.length / 2;
+			while (increment > 0) {
+				for (int i = increment; i < a.length; i++) {
+					int j = i;
+					int temp = a[i];
+					while (j >= increment && a[j - increment] > temp) {
+						a[j] = a[j - increment];
+						j = j - increment;
+					}
+					a[j] = temp;
+				}
+				if (increment == 2) {
+					increment = 1;
+				} else {
+					increment *= (5.0 / 11);
+				}
+			}
+		    }
+
+			// Method to test above
+		    public static void main(String args[])
+		    {
+		    	ShellSort17 ob = new ShellSort17();
+		        int nums[] = {7, -5, 3, 2, 1, 0, 45};
+		        System.out.println("Original Array:");
+		        System.out.println(Arrays.toString(nums));
+		        ob.shell(nums);
+		        System.out.println("Sorted Array:");
+		        System.out.println(Arrays.toString(nums));
+		        }        
+}
